6ee031aef64a85b986f065330f5c60bf92c621a2
xalbizu <xavialbizu@gmail.com>
2023-03-13 16:31:47 +0100

    malloc

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: xalbizu- <xalbizu-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 13:00:53 by xalbizu-          #+#    #+#             */
-/*   Updated: 2023/03/13 13:49:07 by xalbizu-         ###   ########.fr       */
+/*   Updated: 2023/03/13 16:30:23 by xalbizu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 int	main(int argc, char *argv[])
 {
 	struct timeval	start;
+	t_philo			*philosophers;
+	int				num_philosophers;
 
 	gettimeofday(&start, NULL);
 	if (argc < 5 || argc > 6)
@@ -25,6 +27,10 @@ int	main(int argc, char *argv[])
 	}
 	if (!all_arg_are_int(argv))
 		return (0);
+	num_philosophers = ft_atoi(argv[1]);
+	philosophers = malloc(num_philosophers * sizeof(t_philo));
+	if (!philosophers)
+		return (write(1, "Error: unable to allocate memory\n", 33), 0);
 }
 
 long int	elapsed_time(struct timeval start)
@@ -55,4 +61,33 @@ int	all_arg_are_int(char *argv[])
 		}
 	}
 	return (1);
+}
+
+long int	ft_atoi(const char *str)
+{
+	int			count;
+	long int	result;
+	int			isnegative;
+
+	result = 0;
+	isnegative = 0;
+	count = 0;
+	while (str[count] == ' ' || str[count] == '\n' || str[count] == '\r'
+		|| str[count] == '\f' || str[count] == '\t' || str[count] == '\v')
+		count++;
+	if (str[count] == '-' || str[count] == '+')
+	{
+		if (str[count] == '-')
+			isnegative = 1;
+		count++;
+	}
+	while (str[count] >= '0' && str[count] <= '9')
+	{
+		result *= 10;
+		result += str[count] - '0';
+		count++;
+	}
+	if (isnegative == 1)
+		result *= -1;
+	return (result);
 }
