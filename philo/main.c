c4497a511dadf791783edbe40a720cf0822df793
xalbizu <xavialbizu@gmail.com>
2023-03-13 16:43:13 +0100

    Some fixes and cleanup

--- philo/main.c
+++ philo/main.c
@@ -6,11 +6,10 @@
 /*   By: xalbizu- <xalbizu-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 13:00:53 by xalbizu-          #+#    #+#             */
-/*   Updated: 2023/03/13 16:35:20 by xalbizu-         ###   ########.fr       */
+/*   Updated: 2023/03/13 16:40:16 by xalbizu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include "philosophers.h"
 
 int	main(int argc, char *argv[])
@@ -21,16 +20,13 @@ int	main(int argc, char *argv[])
 
 	gettimeofday(&start, NULL);
 	if (argc < 5 || argc > 6)
-	{
-		write(1, "Error: wrong number of arguments\n", 34);
-		return (0);
-	}
+		return (write(1, "Error: wrong number of arguments\n", 34), 0);
 	if (!all_arg_are_int(argv))
 		return (0);
 	num_philosophers = ft_atoi(argv[1]);
 	philosophers = malloc(num_philosophers * sizeof(t_philo));
 	if (!philosophers)
-		return (write(1, "Error: unable to allocate memory\n", 33), 0);
+		return (write(1, "Error: unable to allocate memory\n", 34), 0);
 }
 
 long int	elapsed_time(struct timeval start)
